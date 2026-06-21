856e71cb3a44fc6f4e24fd34822d35dfa694941b
MohamedAttaMohamed <mohamedatta8921@gmail.com>
2026-06-22 02:04:07 +0300

    Refactor time input to use getTime function

--- Time Addition Structure.c
+++ Time Addition Structure.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Time {
     int hours;
@@ -6,23 +7,29 @@ struct Time {
     int seconds;
 };
 
+void getTime(struct Time *t, const char *label) {
+    char buffer[50];
+    printf("Enter %s (hh:mm:ss): ", label);
+    if (fgets(buffer, sizeof(buffer), stdin)) {
+        // Parse the string into the structure members
+        sscanf(buffer, "%d:%d:%d", &t->hours, &t->minutes, &t->seconds);
+    }
+}
+
 int main() {
     struct Time t1, t2, sum;
 
-    printf("Enter first time (hh mm ss): ");
-    scanf("%d %d %d", &t1.hours, &t1.minutes, &t1.seconds);
-
-    printf("Enter second time (hh mm ss): ");
-    scanf("%d %d %d", &t2.hours, &t2.minutes, &t2.seconds);
+    getTime(&t1, "first time");
+    getTime(&t2, "second time");
 
     sum.seconds = t1.seconds + t2.seconds;
     sum.minutes = t1.minutes + t2.minutes + (sum.seconds / 60);
     sum.hours = t1.hours + t2.hours + (sum.minutes / 60);
 
-    sum.seconds = sum.seconds % 60;
-    sum.minutes = sum.minutes % 60;
+    sum.seconds %= 60;
+    sum.minutes %= 60;
 
-    printf("sum = %d:%d:%d\n", sum.hours, sum.minutes, sum.seconds);
+    printf("sum = %02d:%02d:%02d\n", sum.hours, sum.minutes, sum.seconds);
 
     return 0;
 }
