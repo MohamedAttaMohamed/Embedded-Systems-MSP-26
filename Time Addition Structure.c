c79547c2f8951fdaddb57d30f9868399a9154c61
MohamedAttaMohamed <mohamedatta8921@gmail.com>
2026-06-25 23:03:01 +0300

    Refactor time input and output handling
    Updated input format for time entry and adjusted output format.

--- Time Addition Structure.c
+++ Time Addition Structure.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Time {
     int hours;
@@ -7,29 +6,25 @@ struct Time {
     int seconds;
 };
 
-void getTime(struct Time *t, const char *label) {
-    char buffer[50];
-    printf("Enter %s (hh:mm:ss): ", label);
-    if (fgets(buffer, sizeof(buffer), stdin)) {
-        // Parse the string into the structure members
-        sscanf(buffer, "%d:%d:%d", &t->hours, &t->minutes, &t->seconds);
-    }
-}
-
 int main() {
     struct Time t1, t2, sum;
 
-    getTime(&t1, "first time");
-    getTime(&t2, "second time");
+    printf("Enter first time (hh mm ss): ");
+    scanf("%d %d %d", &t1.hours, &t1.minutes, &t1.seconds);
 
+    printf("Enter second time (hh mm ss): ");
+    scanf("%d %d %d", &t2.hours, &t2.minutes, &t2.seconds);
+
+    // Calculate sum of seconds, minutes, and hours
     sum.seconds = t1.seconds + t2.seconds;
     sum.minutes = t1.minutes + t2.minutes + (sum.seconds / 60);
     sum.hours = t1.hours + t2.hours + (sum.minutes / 60);
 
-    sum.seconds %= 60;
-    sum.minutes %= 60;
+    // Keep seconds and minutes within 0-59 range
+    sum.seconds = sum.seconds % 60;
+    sum.minutes = sum.minutes % 60;
 
-    printf("sum = %02d:%02d:%02d\n", sum.hours, sum.minutes, sum.seconds);
+    printf("sum = %d:%d:%d\n", sum.hours, sum.minutes, sum.seconds);
 
     return 0;
 }
