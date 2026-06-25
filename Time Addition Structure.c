83e9d283495c35d54a649e46a8c121552c030f48
MohamedAttaMohamed <mohamedatta8921@gmail.com>
2026-06-25 23:03:40 +0300

    Update Time Addition Structure.c

--- Time Addition Structure.c
+++ Time Addition Structure.c
@@ -15,12 +15,10 @@ int main() {
     printf("Enter second time (hh mm ss): ");
     scanf("%d %d %d", &t2.hours, &t2.minutes, &t2.seconds);
 
-    // Calculate sum of seconds, minutes, and hours
     sum.seconds = t1.seconds + t2.seconds;
     sum.minutes = t1.minutes + t2.minutes + (sum.seconds / 60);
     sum.hours = t1.hours + t2.hours + (sum.minutes / 60);
 
-    // Keep seconds and minutes within 0-59 range
     sum.seconds = sum.seconds % 60;
     sum.minutes = sum.minutes % 60;
 
